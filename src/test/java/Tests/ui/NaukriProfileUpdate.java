c424ba76641b5f2c5007938def11bfca6d1acf69
Nikhil Garg <nikhilgarg855@gmail.com>
2025-06-11 11:21:01 +0530

    fixing workflow

--- src/test/java/Tests/ui/NaukriProfileUpdate.java
+++ src/test/java/Tests/ui/NaukriProfileUpdate.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 public class NaukriProfileUpdate extends BaseTests {
     WebDriver driver;
 
-    NaukriProfileUpdate(){
+    public NaukriProfileUpdate(){
         this.driver = DriverManager.getDriver();
     }
 
